763afeb06c3303309756942f2b0798347ccb2923
Maksofer-PC <maksofer@gmail.com>
2021-06-05 14:01:28 +0300

    first commit

---
+++ Task3/Massiv.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    class Massiv
+    {
+        public int[] posNum;
+        public int i = 0;
+        public int Lenght
+        {
+            set
+            {
+                posNum = new int[value];
+            }
+        }
+        
+        
+        
+
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. Найти произведение положительных элементов одномерного массива A размера N.
+            int n, num, sum = 1;
+            Massiv mas = new Massiv();
+            Console.Write("Введите размер массива: ");
+            n = int.Parse(Console.ReadLine());
+            mas.Lenght = n;
+            Console.WriteLine("Заполните массив числами");
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("");
+                num = int.Parse(Console.ReadLine());
+                mas.posNum[i] = num;
+                if (num > 0)
+                {
+                    sum *= num;
+                }
+                
+            }
+            Console.WriteLine("Произведение всех положительных чисел в одномерном массиве: {0}", sum);
+            
+           
+        }
+    }
+}
